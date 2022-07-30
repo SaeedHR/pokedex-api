073047c1078b2d50b5c8a461c40351c79cd366c0
Mousa <git.ccn@gmail.com>
2022-07-30 19:37:48 +0200

    Add fetch get API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-"use strict";
+{
+    "use strict";
 
-// General CONST
-const INPUT = document.querySelector(".searchBox input");
-const POCKEMONBOX = document.querySelector(".pokemonBox");
-const UL = document.querySelector(".searchBox ul");
+    // General CONST
+    const INPUT = document.querySelector(".searchBox input");
+    const POCKEMONBOX = document.querySelector(".pokemonBox");
+    const UL = document.querySelector(".searchBox ul");
+
+    // request Fetch
+    let requestFetch = pokemonName => {
+        fetch(`https:pokeapi.co/api/v2/pokemon/`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch((err) => {
+                POCKEMONBOX.innerHTML = `
+            <h4>Pokemon not found 😞</h4>
+            `;
+                console.log("Pokemon not found", err);
+            })
+
+    }
+    requestFetch("")
+}
