ffb8948dbcdaa421fe2bf6ce570ae0ef8368b41b
Mousa <git.ccn@gmail.com>
2022-07-30 19:35:20 +0200

    Add general constant

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// General CONST
+const INPUT = document.querySelector(".searchBox input");
+const POCKEMONBOX = document.querySelector(".pokemonBox");
+const UL = document.querySelector(".searchBox ul");
